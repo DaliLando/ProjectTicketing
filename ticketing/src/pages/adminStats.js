2404fbbacefb8e0b31a441ce4c63afa5bb7ce9ed
DaliLando <landolsimohamedali@gmail.com>
2024-08-15 23:39:53 +0100

    style changes to stats page

--- ticketing/src/pages/adminStats.js
+++ ticketing/src/pages/adminStats.js
@@ -47,12 +47,16 @@ const AdminStats = () => {
       <Accordion defaultActiveKey="0">
         {events.map((item, index) => (
           <Accordion.Item eventKey={index.toString()} key={index}>
-            <Accordion.Header>{item.name}</Accordion.Header>
+            <Accordion.Header>
+              <div style={{ width: "100%", textAlign: "center" }}>
+                {item.name}
+              </div>
+            </Accordion.Header>
             <Accordion.Body>
-              <ListGroup variant="flush">
+              <ListGroup variant="flush" style={{ textAlign: "center" }}>
                 {item.nbrSold.map((el, indice) => (
-                  <ListGroup.Item key={indice} style={{ backgroundColor: "#2c2c2c", color: "#ddd", border: "none" }}>
-                    <h5 style={{ color: "#ffffff", marginBottom: "10px" }}>{el.seatType}</h5>
+                  <ListGroup.Item key={indice} style={{ backgroundColor: "#2c2c2c", color: "#ddd", border: "none", textAlign: "center" }}>
+                    <h5 style={{ color: "#ffffff", marginBottom: "10px",textTransform: "uppercase" }}>{el.seatType}</h5>
                     <div>
                       Tickets Sold: <Badge bg="primary">{el.quantity}</Badge>
                       <br />
